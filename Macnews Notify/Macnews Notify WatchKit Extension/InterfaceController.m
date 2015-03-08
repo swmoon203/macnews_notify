8a17ef2a68997bd30946746ccf1455c25f05425b
Sung Wook Moon <mtjddnr@gmail.com>
2015-03-08 14:02:29 -0500

    init WatchKit

---
+++ Macnews Notify/Macnews Notify WatchKit Extension/InterfaceController.m
@@ -0,0 +1,38 @@
+//
+//  InterfaceController.m
+//  Macnews Notify WatchKit Extension
+//
+//  Created by mtjddnr on 2015. 3. 8..
+//  Copyright (c) 2015년 mtjddnr. All rights reserved.
+//
+
+#import "InterfaceController.h"
+
+
+@interface InterfaceController()
+
+@end
+
+
+@implementation InterfaceController
+
+- (void)awakeWithContext:(id)context {
+    [super awakeWithContext:context];
+
+    // Configure interface objects here.
+}
+
+- (void)willActivate {
+    // This method is called when watch view controller is about to be visible to user
+    [super willActivate];
+}
+
+- (void)didDeactivate {
+    // This method is called when watch view controller is no longer visible
+    [super didDeactivate];
+}
+
+@end
+
+
+
