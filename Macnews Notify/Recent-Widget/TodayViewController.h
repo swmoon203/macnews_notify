a81b2ffb465d640dff9dc49b3cac3f4aeb3d3a6c
Sung Wook Moon <mtjddnr@gmail.com>
2015-03-17 00:15:53 -0400

    widget init

---
+++ Macnews Notify/Recent-Widget/TodayViewController.h
@@ -0,0 +1,13 @@
+//
+//  TodayViewController.h
+//  Recent-Widget
+//
+//  Created by mtjddnr on 2015. 3. 17..
+//  Copyright (c) 2015년 mtjddnr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TodayViewController : UIViewController
+
+@end
