f7566811ccbbff579862e7a5a458d40f29a3b811
Sung Wook Moon <mtjddnr@gmail.com>
2015-03-19 22:56:48 -0400

    Removed update data from widget

--- Macnews Notify/Recent-Widget/TodayViewController.m
+++ Macnews Notify/Recent-Widget/TodayViewController.m
@@ -22,16 +22,12 @@
 @end
 
 @implementation TodayViewController {
-    NSInteger _idx;
+    id _current;
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
-    
     NSLog(@"viewDidLoad");
-    
-    [self updateData];
     [self updateScreen];
 }
 
@@ -61,73 +57,7 @@
     
     return _fetchedResultsController;
 }
-- (BOOL)updateData {
-    NSUserDefaults *userDefaults = [[NSUserDefaults alloc] initWithSuiteName:@"group.kr.smoon.ios.macnews"];
-    NSInteger idx = [userDefaults integerForKey:@"idx"];
-    NSString *token = [userDefaults stringForKey:@"deviceToken"];
-    
-    NSLog(@"Start Loading %li", (long)idx);
-    
-    NSString *url = token != nil ? [NSString stringWithFormat:@"https://push.smoon.kr/v1/notification/%@/%li", token, (long)idx] :
-    [NSString stringWithFormat:@"https://push.smoon.kr/v1/notification/%li", (long)idx];
-        
-    NSURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:url]];
-    NSURLResponse *response = nil;
-    NSError *error = nil, *errorJson = nil;
-    NSData *data = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
-        
-    if ([(NSHTTPURLResponse *)response statusCode] != 200) {
-        NSLog(@"Error Loading");
-        return NO;
-    }
-        
-    NSArray *json = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:&errorJson];
-    NSMutableArray *list = [NSMutableArray array];
-    for (NSDictionary *obj in json) {
-        NSMutableDictionary *item = [NSMutableDictionary dictionaryWithDictionary:obj];
-        item[@"reg"] = [NSDate dateWithTimeIntervalSince1970:[item[@"reg"] intValue]];
-        NSDictionary *apn = [NSJSONSerialization JSONObjectWithData:[item[@"contents"] dataUsingEncoding:NSUTF8StringEncoding] options:kNilOptions error:nil];
-        apn = apn[@"apn"];
-        item[@"title"] = apn[@"title"];
-        if (apn[@"image"]) item[@"image"] = apn[@"image"];
-        if ([apn[@"url-args"] count] > 0) item[@"arg"] = apn[@"url-args"][0];
-        [list addObject:item];
-        idx = MAX(idx, [item[@"idx"] integerValue]);
-        NSLog(@"+idx:%lu", idx);
-    }
-        
-    NSManagedObjectContext *context = [self.fetchedResultsController managedObjectContext];
-    NSEntityDescription *entity = [[self.fetchedResultsController fetchRequest] entity];
-        
-    NSLog(@"Downloaded: %lu", (unsigned long)[list count]);
-    for (NSDictionary *item in list) {
-        NSManagedObject *newManagedObject = [NSEntityDescription insertNewObjectForEntityForName:[entity name] inManagedObjectContext:context];
-        [newManagedObject setValuesForKeysWithDictionary:item];
-        [newManagedObject setValue:@NO forKey:@"archived"];
-        
-        if ([newManagedObject valueForKey:@"image"] != nil && idx == [item[@"idx"] integerValue]) {
-            NSData *imageData = [NSData dataWithContentsOfURL:[NSURL URLWithString:[newManagedObject valueForKey:@"image"]]];
-            if (imageData != nil) {
-                [newManagedObject setValue:imageData forKey:@"imageData"];
-            }
-        }
-    }
-    
-    NSError *dbError = nil;
-    if (![context save:&dbError]) {
-        // Replace this implementation with code to handle the error appropriately.
-        // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-        NSLog(@"Unresolved error %@, %@", dbError, [dbError userInfo]);
-        abort();
-    }
-    
-    [userDefaults setInteger:idx forKey:@"idx"];
-    [userDefaults synchronize];
-    
-    NSLog(@"End Loading %li", (long)idx);
-    
-    return [list count] != 0;
-}
+
 - (BOOL)updateScreen {
     id <NSFetchedResultsSectionInfo> sectionInfo = [self.fetchedResultsController sections][0];
     NSLog(@"[sectionInfo numberOfObjects] == %lu", (unsigned long)[sectionInfo numberOfObjects]);
@@ -136,11 +66,11 @@
         self.lblBody.hidden = YES;
         return NO;
     }
-    self.lblBody.hidden = NO;
     id item = [self.fetchedResultsController objectAtIndexPath:[NSIndexPath indexPathForRow:0 inSection:0]];
     
-    if (_idx == [[item valueForKey:@"idx"] integerValue]) return NO;
-        
+    if ([[_current valueForKey:@"idx"] integerValue] == [[item valueForKey:@"idx"] integerValue] && [item valueForKey:@"imageData"] == nil) return NO;
+    
+    self.lblBody.hidden = NO;
     
     self.lblTitle.text = [item valueForKey:@"title"];
     
@@ -150,7 +80,7 @@
     if ([item valueForKey:@"imageData"] != nil) {
         self.imageView.image = [UIImage imageWithData:[item valueForKey:@"imageData"]];
     }
-    _idx = [[item valueForKey:@"idx"] integerValue];
+    _current = item;
     return YES;
 }
 
@@ -161,7 +91,7 @@
 
 - (void)widgetPerformUpdateWithCompletionHandler:(void (^)(NCUpdateResult))completionHandler {
     NSLog(@"widgetPerformUpdateWithCompletionHandler");
-    completionHandler([self updateData] ? NCUpdateResultNewData : NCUpdateResultNoData);
+    completionHandler([self updateScreen] ? NCUpdateResultNewData : NCUpdateResultNoData);
 }
 - (IBAction)onTap:(id)sender {
     if ([[self.fetchedResultsController sections][0] numberOfObjects] == 0) return;
