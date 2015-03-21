84c3b931e3b59ad61c0fa07184ab0af270a0a60e
Sung Wook Moon <mtjddnr@gmail.com>
2015-03-21 13:29:18 -0500

    <MRProgress> project setting && headers

--- Macnews Notify/MRProgress/MRProgress.h
+++ Macnews Notify/MRProgress/MRProgress.h
@@ -16,4 +16,16 @@ FOUNDATION_EXPORT const unsigned char MRProgressVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <MRProgress/PublicHeader.h>
 
+#import <MRProgress/MRActivityIndicatorView.h>
+#import <MRProgress/MRCircularProgressView.h>
+#import <MRProgress/MRIconView.h>
+#import <MRProgress/MRNavigationBarProgressView.h>
+#import <MRProgress/MRProgressOverlayView.h>
 
+#import <MRProgress/MRBlurView.h>
+#import <MRProgress/MRMessageInterceptor.h>
+#import <MRProgress/MRMethodCopier.h>
+#import <MRProgress/MRProgressHelper.h>
+#import <MRProgress/MRStopButton.h>
+#import <MRProgress/MRWeakProxy.h>
+#import <MRProgress/UIImage+MRImageEffects.h>
