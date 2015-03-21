9aa3c8eace481d44b494b7524688984757bb96ae
Sung Wook Moon <mtjddnr@gmail.com>
2015-03-21 13:28:22 -0500

    + MRProgress framework

---
+++ Macnews Notify/MRProgress/MRProgress.h
@@ -0,0 +1,19 @@
+//
+//  MRProgress.h
+//  MRProgress
+//
+//  Created by mtjddnr on 2015. 3. 21..
+//  Copyright (c) 2015년 mtjddnr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MRProgress.
+FOUNDATION_EXPORT double MRProgressVersionNumber;
+
+//! Project version string for MRProgress.
+FOUNDATION_EXPORT const unsigned char MRProgressVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MRProgress/PublicHeader.h>
+
+
