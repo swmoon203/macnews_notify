faa0b3397d1e086e0b547afb3f8905d3a4f48d78
Sung Wook Moon <mtjddnr@gmail.com>
2015-03-23 18:19:04 -0500

    WatchKit Glance view

--- Macnews Notify/Macnews Notify WatchKit Extension/GlanceController.m
+++ Macnews Notify/Macnews Notify WatchKit Extension/GlanceController.m
@@ -7,9 +7,13 @@
 //
 
 #import "GlanceController.h"
+#import <MacnewsCore/MacnewsCore.h>
 
 
 @interface GlanceController()
+@property (weak, nonatomic) IBOutlet WKInterfaceImage *priviewImageView;
+@property (weak, nonatomic) IBOutlet WKInterfaceLabel *titleLabel;
+@property (weak, nonatomic) IBOutlet WKInterfaceLabel *contentLabel;
 
 @end
 
@@ -20,6 +24,7 @@
     [super awakeWithContext:context];
 
     // Configure interface objects here.
+    [self updateScreen];
 }
 
 - (void)willActivate {
@@ -31,7 +36,31 @@
     // This method is called when watch view controller is no longer visible
     [super didDeactivate];
 }
+- (void)updateScreen {
 
+    NSManagedObjectContext *context = [DataStore sharedData].managedObjectContext;
+    NSFetchRequest *fetchRequest = [NSFetchRequest fetchRequestWithEntityName:@"Notification"];
+    [fetchRequest setFetchBatchSize:1];
+    [fetchRequest setFetchLimit:1];
+    [fetchRequest setSortDescriptors:@[ [NSSortDescriptor sortDescriptorWithKey:@"idx" ascending:NO] ]];
+    
+    NSError *err = nil;
+    NSArray *items = [context executeFetchRequest:fetchRequest error:&err];
+    
+    if ([items count] == 0) {
+        [self.titleLabel setText:@"데이터가 없습니다"];
+        return;
+    }
+    NSManagedObject *object = items[0];
+    
+    [self.titleLabel setText:[object valueForKey:@"title"]];
+//    NSData *imageData = [object valueForKey:@"imageData"];
+//    if (imageData) {
+//        [self.priviewImageView setImageData:imageData];
+//    }
+    NSDictionary *json = [NSJSONSerialization JSONObjectWithData:[(NSString *)[object valueForKey:@"contents"] dataUsingEncoding:NSUTF8StringEncoding] options:kNilOptions error:nil];
+    [self.contentLabel setText:json[@"apn"][@"message"]];
+}
 @end
 
 
