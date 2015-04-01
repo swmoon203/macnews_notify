b9d37391ff4be4e8e96eb5e39c1df60e638e2786
Sung Wook Moon <mtjddnr@gmail.com>
2015-04-01 14:54:06 -0500

    Watch App Screen

---
+++ Macnews Notify/Macnews Notify WatchKit Extension/RootInterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  RootInterfaceController.h
+//  Macnews Notify
+//
+//  Created by mtjddnr on 2015. 4. 1..
+//  Copyright (c) 2015년 mtjddnr. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface RootInterfaceController : WKInterfaceController
+
+@end
---
+++ Macnews Notify/Macnews Notify WatchKit Extension/RootInterfaceController.m
@@ -0,0 +1,38 @@
+//
+//  RootInterfaceController.m
+//  Macnews Notify
+//
+//  Created by mtjddnr on 2015. 4. 1..
+//  Copyright (c) 2015년 mtjddnr. All rights reserved.
+//
+
+#import "RootInterfaceController.h"
+
+
+@interface RootInterfaceController()
+
+@end
+
+
+@implementation RootInterfaceController
+
+- (void)awakeWithContext:(id)context {
+    [super awakeWithContext:context];
+    
+    // Configure interface objects here.
+}
+
+- (void)willActivate {
+    // This method is called when watch view controller is about to be visible to user
+    [super willActivate];
+}
+
+- (void)didDeactivate {
+    // This method is called when watch view controller is no longer visible
+    [super didDeactivate];
+}
+
+@end
+
+
+
